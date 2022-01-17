4d65f789c242e813ddb29c1badcb62197a56a1a4
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-01-16 21:23:05 -0500

    Add files via upload

---
+++ Keeping Laptop Awake.py
@@ -0,0 +1,10 @@
+import time 
+  
+# a module which has functions related to time.  
+# It can be installed using cmd command:  
+# pip install time, in the same way as pyautogui. 
+import pyautogui 
+time.sleep(0)  
+pyautogui.PAUSE = 1
+while (1):
+    pyautogui.press('numlock')
